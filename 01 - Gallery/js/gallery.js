27a8d10f781750a7c09e074c1798ef962812c3b7
iirenesg <irenepsg10@gmail.com>
2017-08-16 00:41:47 -0600

    Agrego tarea 1

---
+++ 01 - Gallery/js/gallery.js
@@ -0,0 +1,75 @@
+function Gallery(images) {
+	var self = this;
+
+	self.amtItems = images.length;
+	self.currentIndex = 0;
+	self.imagesSrc = images;
+
+	self.container = document.getElementById('gallery-container');
+	self.nextBtn = document.getElementById('btn-next');
+	self.prevBtn = document.getElementById('btn-prev');
+	
+}
+
+Gallery.prototype = {
+	constructor: Gallery,
+	self: null,
+
+	/**
+	 * Initializes the gallery
+	 */
+	init: function() {
+		self = this;
+		self.nextBtn.addEventListener('click', self.setNext);
+		self.prevBtn.addEventListener('click', self.setPrev);
+		self.composeGallery();
+	},
+
+	/**
+	 * Composes the gallery in the initialization
+	 */
+	composeGallery: function() {
+
+		var ul = document.createElement('ul');
+		var li = document.createElement('li');
+			
+		var img = document.createElement('img');
+		img.setAttribute('src', self.imagesSrc[self.currentIndex]);
+
+		ul.appendChild(li);
+		li.appendChild(img);
+		self.container.appendChild(ul);
+	},
+
+	/**
+	 * Sets the next slide
+	 */
+	setNext: function() {
+		if (self.currentIndex == self.amtItems-1) {
+			self.currentIndex = 0;
+		}
+
+		var items = self.container.getElementsByTagName('li');
+		for (var i=0; i<items.length; i++) {
+			var img = items[i].getElementsByTagName('img');	
+			self.currentIndex++;
+			img[0].setAttribute('src', self.imagesSrc[self.currentIndex]);
+		}
+	},
+
+	/**
+	 * Sets the previous slide
+	 */
+	setPrev: function() {
+		if (self.currentIndex == 0) {
+			self.currentIndex = self.amtItems-1;
+		}
+
+		var items = self.container.getElementsByTagName('li');
+		for (var i=0; i<items.length; i++) {
+			var img = items[i].getElementsByTagName('img');
+			self.currentIndex--;
+			img[0].setAttribute('src', self.imagesSrc[self.currentIndex]);
+		}
+	}
+}
---
+++ 01 - Gallery/js/main.js
@@ -0,0 +1,23 @@
+var imgArray = [
+	"../GalleryAssets/img/1406965068.jpg",
+	"../GalleryAssets/img/1439358068.jpg",
+	"../GalleryAssets/img/1443358068.jpg",
+	"../GalleryAssets/img/1451415912.jpg",
+	"../GalleryAssets/img/1451416904.jpg",
+	"../GalleryAssets/img/1451431276.jpg",
+	"../GalleryAssets/img/1451433043.jpg",
+	"../GalleryAssets/img/1452034116.jpg",
+	"../GalleryAssets/img/1464358068.jpg",
+	"../GalleryAssets/img/1469328068.jpg",
+	"../GalleryAssets/img/1469355068.jpg",
+	"../GalleryAssets/img/1469358068.jpg",
+	"../GalleryAssets/img/1469358168.jpg",
+	"../GalleryAssets/img/1469358268.jpg",
+	"../GalleryAssets/img/1469444877.jpg",
+	"../GalleryAssets/img/1469654367.jpg",
+	"../GalleryAssets/img/1469735732.jpg",
+	"../GalleryAssets/img/1487358068.jpg"
+]
+
+var gallery = new Gallery(imgArray);
+gallery.init();
